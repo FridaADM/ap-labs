e15c7da72bd76586aa9665c7c575a2eb831c521a
Frida Diaz <fridaadm@gmail.com>
2019-09-24 21:50:24 -0500

    String manipulation library
    Signed-off-by: Frida Diaz <fridaadm@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char** argv) {
+    if(argc != 4){
+        exit(-1);
+    }
+
+    int strlen = mystrlen(argv[1]);
+    char* newstr = mystradd(argv[1],argv[2]);
+    printf("Original string length: %d\n",strlen);
+    printf("New string: %s\n",newstr);
+    int *found;
+    printf("Substring was found : %s\n", found = mystrfind(newstr,argv[3])? "yes":"no");
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,38 @@
 
 
-int mystrlen(char *str){
-    return 0;
-}
-
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int x = mystrlen(origin);                 
+    int y = mystrlen(addition);               
+    char *new = malloc((x+y)*sizeof(char));  
+    for(int i=0; i<x; i++){                         
+        new[i] = origin[i];                   
+    }
+    for(int j=0; j<y; j++){
+        new[i++] = addition[j];               
+    }
+    return new;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int x = mystrlen(origin);                 
+    int y = mystrlen(substr);
+    int val = 0;
+    for(int i=0; i<x; i++){
+        if(origin[i] == substr[0]) {
+             val = 1;
+	     for(int j=1; j<b; j++){
+                if(origin[i+j] != substr[j]){
+                    val = 0;
+                    break;
+                }
+                else if(!val){
+                    val = 1;
+                }
+            }
+            if(val){
+                break;
+            }
+        }
+    } 
+    return val;
 }
