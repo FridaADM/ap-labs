3f33f6df6da31e26838ab6917c40c8c19326a86a
Frida Diaz <fridaadm@gmail.com>
2020-03-03 22:38:23 -0600

    Solve go-slices
    Signed-off-by: Frida Diaz <fridaadm@gmail.com>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,19 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dx)
+    for x := range a {
+        a[x] = make([]uint8, dy)
+        for y := range a[x] {
+        a[x][y] = uint8(x)
+        }
+    }
+    return a
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
