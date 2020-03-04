4576aea9cf0fcceaa7279ce215035507c2d45b9b
Frida Diaz <fridaadm@gmail.com>
2020-03-03 22:36:19 -0600

    Solve go-maps
    Signed-off-by: Frida Diaz <fridaadm@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,21 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	mapWithWords := make( map[string]int)
+	words := strings.Fields(s)
+	
+	for _,word := range words{
+		mapWithWords[word]++
+	}
+	return mapWithWords
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
